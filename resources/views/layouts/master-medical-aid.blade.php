b7a76cb07027fa28a4adcd3435fb1bd3e83b915d
Neo Mokoena <mokoena.n.a@gmail.com>
2017-05-15 10:14:24 +0200

    Add dropdowm menu

--- resources/views/layouts/master-medical-aid.blade.php
+++ resources/views/layouts/master-medical-aid.blade.php
@@ -65,25 +65,74 @@
                                 <i class="fa fa-ellipsis-v"></i>
                             </button>
                         </div>
-                        <nav class="c-mega-menu c-pull-right c-mega-menu-dark c-mega-menu-dark-mobile c-fonts-uppercase c-fonts-bold">
+                        <nav class="c-mega-menu c-pull-right c-mega-menu-light c-mega-menu-light-mobile  c-fonts-uppercase c-fonts-bold">
                             <ul class="nav navbar-nav c-theme-nav">
-                                <li class="c-active">
+                             <li class="c-active">
+                                    <a href="javascript:;" class="c-link dropdown-toggle">Home
+                                    </a>
+                                </li>
+                              <li class="c-menu-type-classic">
                                     <a href="javascript:;" class="c-link dropdown-toggle">Individuals
+                                        <span class="c-arrow c-toggler"></span>
                                     </a>
+                                    <ul class="dropdown-menu c-menu-type-classic c-pull-left">
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">I Want To...
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Lodge a claim 
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Find a Medical Doctor
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Request a Quotation
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Login to my Account
+                                            </a>
+                                        </li>
+
+                                    </ul>
                                 </li>
-                                <li class="c-active">
+                              <li class="c-menu-type-classic">
                                     <a href="javascript:;" class="c-link dropdown-toggle">Employers
+                                        <span class="c-arrow c-toggler"></span>
                                     </a>
+                                    <ul class="dropdown-menu c-menu-type-classic c-pull-left">
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">I Want To...
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Request a Quotation
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Download Forms
+                                            </a>
+                                        </li>
+                                        <li class="dropdown-submenu">
+                                            <a href="javascript:;">Login to my Account
+                                            </a>
+                                        </li>
+
+                                    </ul>
                                 </li>
-                                <li class="c-active">
+
+                                <li class="">
                                     <a href="javascript:;" class="c-link dropdown-toggle">Healthcare Providers 
                                     </a>
                                 </li>
-                                <li class="c-active">
+                                <li class="">
                                     <a href="javascript:;" class="c-link dropdown-toggle">Partners
                                     </a>
                                 </li>
-                                <li class="c-active">
+                                <li class="">
                                     <a href="javascript:;" class="c-link dropdown-toggle">Member Log-In
                                     </a>
                                 </li>
