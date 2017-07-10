94ff257b1de237e9d5440e875e6d54b6ed5218f7
Neo Mokoena <mokoena.n.a@gmail.com>
2017-07-10 10:11:55 +0200

    Deploy: Add Gold Color

--- resources/views/medical/member/become-a-member.blade.php
+++ resources/views/medical/member/become-a-member.blade.php
@@ -84,7 +84,7 @@
                         <div class="c-content-step-1 c-opt-1">
                             <div class="c-icon">
                                 <span class="c-hr c-hr-first step-container">
-                                            <span class="step">5</span>
+                                            <span class="step">6</span>
                                 </span>
                             </div>
                             <div class="c-title c-font-20 c-font-bold c-font-uppercase">Serve waiting period</div>
